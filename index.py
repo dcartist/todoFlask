30af08d5ee60ad9a6c5ba1a059b5981f2dc761d0
dcartist <pbannerman13@gmail.com>
2021-12-05 12:36:01 -0500

    updating index

--- index.py
+++ index.py
@@ -7,12 +7,18 @@ todoList = ["Clean Kitchen", "Eat Food", "Code"]
 @app.route("/", methods=['GET'])
 def index():
     return render_template('index.html', todo=todoList)
+
+
+################TODO ADD Function
 @app.route("/add/", methods=['POST'])
 def postTodo():
     # print(request.form['name'])
     todoList.append(request.form['name'])
     return redirect("/")
     # return render_template('index.html', todo=todoList)
+
+
+################TODO Show individual item Function
 @app.route("/id/<id>", methods=['GET', 'POST'])
 def editInfo(id=None):
     if request.method =='GET':
@@ -21,7 +27,7 @@ def editInfo(id=None):
     number = int(id)
     return render_template("edit.html", item=todoList[number], id=id )
 
-
+################TODO EDIT Function
 @app.route("/edit", methods=['GET', 'POST'])
 def updateInfo():
     print(request.form)
@@ -30,6 +36,14 @@ def updateInfo():
     todoList[number] = request.form['name']
     path = f"/id/{request.form['id']}"
     return redirect(path)
+
+
+################TODO DELETE Function
+@app.route("/delete/<id>", methods=["GET"])
+def deleteItem(id=None):
+    number = int(id)
+    todoList.pop(number)
+    return redirect("/")
 
 ################* Run App
 app.run(port=3000, debug=True)
